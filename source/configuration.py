bdfcdb65f9c789f8552a93b18f5f18f4fd9a865e
rafal <rafal.rolczynski@gmail.com>
2019-08-10 16:00:34 +0200

    Abstract ModelConfiguration and DatasetConfiguration

--- source/configuration.py
+++ source/configuration.py
@@ -1,24 +1,15 @@
 import yaml
 from typing import List, Dict
+from source.audio import FeaturesExtractor
+from source.text import Alphabet
 
 
 class Configuration:
-    """
-    Each experiment has own experiment.yaml configuration file. This
-    configuration object passes through all methods which required
-    additional parameters.
-    """
 
     def __init__(self, file_path: str):
         """ All parameters saved in .yaml file convert to dot accessible """
         self._file_path = file_path
         self._data = self._read_yaml_file()
-        self._check_file(required_keys=['features_extractor', 'model', 'callbacks', 'optimizer', 'decoder'])
-        self.features_extractor = self._data.get('features_extractor')
-        self.model = self._data.get('model')
-        self.callbacks = self._data.get('callbacks')
-        self.optimizer = self._data.get('optimizer')
-        self.decoder = self._data.get('decoder')
 
     @property
     def data(self):
@@ -32,3 +23,36 @@ class Configuration:
     def _check_file(self, required_keys: List[str]):
         if not all(key in self._data for key in required_keys):
             raise KeyError(f'Configuration file should have all required keys: {required_keys}')
+
+
+class ModelConfiguration(Configuration):
+    """
+    Each DeepSpeech model has own model.yaml configuration file. This
+    configuration object passes through all methods required to build
+    DeepSpeech object.
+    """
+
+    def __init__(self, file_path: str):
+        super().__init__(file_path)
+        self._check_file(required_keys=['features_extractor', 'model', 'callbacks', 'optimizer', 'decoder'])
+        self.features_extractor = self._data.get('features_extractor')
+        self.model = self._data.get('model')
+        self.callbacks = self._data.get('callbacks')
+        self.optimizer = self._data.get('optimizer')
+        self.decoder = self._data.get('decoder')
+
+
+class DatasetConfiguration(Configuration):
+
+    def __init__(self, file_path: str, alphabet: Alphabet, features_exractor: FeaturesExtractor):
+        super().__init__(file_path)
+        self.alphabet = alphabet
+        self.feature_extractor = features_exractor
+        self._check_file(required_keys=['constructor', 'source', 'parameters'])
+        constructor_name = self._data.get('constructor')
+        source = self._data.get('source')
+        self.constructor = getattr(constructor_name, source)
+        self.parameters = self._data.get('parameters')
+
+    def create_generator(self):
+        return self.constructor(**self.parameters)
